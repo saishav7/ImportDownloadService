e3f2317ada2c944af7d62c914a0c92e90d1d45ae
hariAtThinkBright <hari@thinkbrightmedia.com.au>
2014-09-10 20:16:07 +1000

    Added another Skeleton with downloadFile

---
+++ src/au/edu/unsw/sltf/services/ImportDownloadServicesSkeleton1.java
@@ -0,0 +1,84 @@
+
+/**
+ * ImportDownloadServicesSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package au.edu.unsw.sltf.services;
+
+import au.edu.unsw.sltf.services.ImportMarketDataDocument.ImportMarketData;
+import au.edu.unsw.sltf.services.ImportMarketDataResponseDocument.ImportMarketDataResponse;
+    /**
+     *  ImportDownloadServicesSkeleton java skeleton for the axisService
+     */
+    public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSkeletonInterface{
+
+        private String resourcesFolder = "/home/hari/apache-tomcat-7.0.42/webapps/ROOT/cs9322ass1/";
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+         * @param importMarketData0 
+         * @return importMarketDataResponse1 
+         * @throws ImportDownloadFaultException 
+         */
+        
+         public au.edu.unsw.sltf.services.ImportMarketDataResponseDocument importMarketData
+          (
+          au.edu.unsw.sltf.services.ImportMarketDataDocument importMarketData0
+          ) throws ImportDownloadFaultException{
+                //TODO : fill this with the necessary business logic
+        	 ImportMarketData req = importMarketData0.getImportMarketData();
+             StringBuilder sbf = new StringBuilder();
+             sbf.append("Security Code: ").append(req.getSec()).append("\r\n");
+             sbf.append("Start date: ").append(req.getStartDate()).append("\r\n");
+             sbf.append("End date: ").append(req.getEndDate()).append("\r\n");
+             sbf.append("Data source: ").append(req.getDataSourceURL()).append("\r\n");
+             String returnStr = sbf.toString();
+
+             ImportMarketDataResponseDocument resDoc = ImportMarketDataResponseDocument.Factory.newInstance();
+             ImportMarketDataResponse res = resDoc.addNewImportMarketDataResponse();
+             res.setReturn(returnStr);
+
+             return resDoc;
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+         * @param downloadFile2 
+         * @return downloadFileResponse3 
+         * @throws ImportDownloadFaultException 
+         */
+        
+         public au.edu.unsw.sltf.services.DownloadFileResponseDocument downloadFile
+          (
+          au.edu.unsw.sltf.services.DownloadFileDocument downloadFile2
+          )
+            throws ImportDownloadFaultException{
+                //TODO : fill this with the necessary business logic
+                //throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#downloadFile");
+                DownloadFile dfreq = downloadFile2.getDownloadFile();
+                     
+                String returnStr = "EventSet Id: " + dfreq.getEventSetID();
+
+                String url = resourcesFolder + "/" + returnStr;
+                File f = new File(url);
+
+                DownloadFileResponseDocument dfrespdoc = DownloadFileResponseDocument.Factory.newInstance();
+                DownloadFileResponse dfresp = dfrespdoc.addNewDownloadFileResponse();
+                if (f.exists()) {
+                    dfresp.setDataURL(url);
+                } else {
+                    //does not exist
+                }
+                 
+                return dfrespdoc;
+    }
+        }
+     
+    }
+    
